35a92ccc3ff5c9f6db0505bf6b831f6ce2fe92b7
20122000-fjh <franhillebrans@gmail.com>
2021-06-14 12:22:21 -0300

    exámen módulo 4

---
+++ examenmodulo4/SpeakGoodBye.js
@@ -0,0 +1,25 @@
+// ¡NOTA! Los pasos de este archivo son básicamente idénticos a los que
+// realizado en el archivo SpeakHello.js.
+
+// PASO 6: Envuelva todo el contenido de SpeakGoodBye.js dentro de un IIFE
+// Ver Clase 52, parte 2
+
+
+// PASO 7: Cree un objeto, llamado 'byeSpeaker' al que adjuntará
+// el método "speak" y que expondrá al contexto global
+// Ver Clase 52, parte 1
+// var byeSpeaker =
+
+// NO adjunte la variable speakWord al objeto 'byeSpeaker'.
+var  speakWord  =  "Adiós" ;
+
+// PASO 8: Vuelva a escribir la función 'hablar' de modo que esté adjunta a la
+// objeto byeSpeaker en lugar de ser una función independiente.
+// Ver Clase 52, parte 2
+función  hablar ( nombre )  {
+  consola . log ( speakWord  +  ""  +  nombre ) ;
+}
+
+// PASO 9: Exponga el objeto 'byeSpeaker' al alcance global. Nombralo
+// 'byeSpeaker' también en el ámbito global.
+// xxxx.xxxx = byeSpeaker;
