8ea09580852ad44b4b789818d9b285e1d87a0f05
20122000-fjh <franhillebrans@gmail.com>
2021-06-21 11:31:04 -0300

    examen terminado

--- examenmodulo4/SpeakGoodBye.js
+++ examenmodulo4/SpeakGoodBye.js
@@ -3,21 +3,28 @@
 
 // PASO 6: Envuelva todo el contenido de SpeakGoodBye.js dentro de un IIFE
 // Ver Clase 52, parte 2
+(function (window) {
+  // PASO 7: Cree un objeto, llamado 'byeSpeaker' al que adjuntará
+  // el método "speak" y que expondrá al contexto global
+  // Ver Clase 52, parte 1
+  // var byeSpeaker =
+  var byeSpeaker = {};
+  // NO adjunte la variable speakWord al objeto 'byeSpeaker'.
+  var speakWord = "Good Bye";
 
-// PASO 7: Cree un objeto, llamado 'byeSpeaker' al que adjuntará
-// el método "speak" y que expondrá al contexto global
-// Ver Clase 52, parte 1
-// var byeSpeaker =
+  // PASO 8: Vuelva a escribir la función 'hablar' de modo que esté adjunta a la
+  // objeto byeSpeaker en lugar de ser una función independiente.
+  // Ver Clase 52, parte 2
+  byeSpeaker.speak = function speak(name) {
+    console.log(speakWord + "" + name);
+  };
 
-// NO adjunte la variable speakWord al objeto 'byeSpeaker'.
-var speakWord = "Good Bye";
+  window.byeSpeaker = byeSpeaker;
 
-// PASO 8: Vuelva a escribir la función 'hablar' de modo que esté adjunta a la
-// objeto byeSpeaker en lugar de ser una función independiente.
-// Ver Clase 52, parte 2
-function speak(name) {
-  console.log(speakWord + "" + name);
-}
+  function speak(name) {
+    console.log(speakWord + "" + name);
+  }
+})(window);
 
 // PASO 9: Exponga el objeto 'byeSpeaker' al alcance global. Nombralo
 // 'byeSpeaker' también en el ámbito global.
--- examenmodulo4/SpeakHello.js
+++ examenmodulo4/SpeakHello.js
@@ -4,17 +4,25 @@
 // PASO 3: Crea un objeto, llamado 'helloSpeaker' al que adjuntarás
 // el método "speak" y que expondrá al contexto global
 // Ver Clase 52, parte 1
-// var helloSpeaker =
 
-// NO adjunte la variable speakWord al objeto 'helloSpeaker'.
-var speakWord = "Hello";
+(function (window) {
+  var helloSpeaker = {};
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
 
-// PASO 4: Vuelva a escribir la función 'hablar' de modo que esté adjunta a la
-// objeto helloSpeaker en lugar de ser una función independiente.
-// Ver Clase 52, parte 2
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+  window.helloSpeaker = helloSpeaker;
+  // NO adjunte la variable speakWord al objeto 'helloSpeaker'.
+
+  var speakWord = "Hello";
+
+  // PASO 4: Vuelva a escribir la función 'hablar' de modo que esté adjunta a la
+  // objeto helloSpeaker en lugar de ser una función independiente.
+  // Ver Clase 52, parte 2
+  function speak(name) {
+    console.log(speakWord + " " + name);
+  }
+})(window);
 
 // PASO 5: Exponga el objeto 'helloSpeaker' al alcance global. Nombralo
 // 'helloSpeaker' también en el ámbito global.
